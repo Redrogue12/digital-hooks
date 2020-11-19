f623e4b8c722b730d6e7d67795c6d27f2881f583
Redrogue12 <exgc12@gmail.com>
2020-11-19 14:48:18 -0400

    access config

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useRecoilValueLoadable } from 'recoil';
 import Activities from './shared-components/Activities';
 import Header from './shared-components/Header';
 import Balances from './shared-components/Balances';
+import { configSelector } from './store/Selectors';
 
 function Home() {
+  const config = useRecoilValueLoadable(configSelector);
+  console.log('config:', config);
   return (
     <>
       <Header />
