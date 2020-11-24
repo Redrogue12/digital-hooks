6b00484b58f74aa4103823b0e533fcb23f0391cd
Redrogue12 <exgc12@gmail.com>
2020-11-24 16:42:45 -0400

    add services + rm admin items

--- src/components/shared-components/sidebar/Sidebar.js
+++ src/components/shared-components/sidebar/Sidebar.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Layout, Menu, Button } from 'antd';
-import { CreditCardOutlined, createFromIconfontCN } from '@ant-design/icons';
+import { CreditCardOutlined, NotificationOutlined } from '@ant-design/icons';
 import { useRecoilState } from 'recoil';
 import { collapseAtom } from './store/Atoms';
 import SidebarDropdown from './SidebarDropdown';
-
-const IconFont = createFromIconfontCN({
-  scriptUrl: ['//at.alicdn.com/t/font_2202999_l9anz3ql6n.js'],
-});
 
 function Sidebar() {
   const [collapsed, setCollapsed] = useRecoilState(collapseAtom);
@@ -29,52 +25,29 @@ function Sidebar() {
           defaultSelectedKeys="stripe"
           className="border-0 flex-grow overflow-auto scrollbar-thin"
         >
-          <Menu.ItemGroup title="Payments">
-            <Menu.Item key="stripe" icon={<IconFont type="icon-ccstripe" />}>
-              Stripe
-            </Menu.Item>
+          <Menu.ItemGroup title="Services">
             <Menu.Item
-              key="paypal"
-              icon={<IconFont type="icon-paypal" />}
-              disabled
+              key="PN"
+              icon={<NotificationOutlined />}
+              onClick={() => console.log('push')}
             >
-              Paypal
+              Push Notifications
             </Menu.Item>
-            <Menu.Item key="ath-movil" icon={<CreditCardOutlined />} disabled>
-              ATH Móvil (soon)
-            </Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Setup">
             <Menu.Item
-              key="settings-store"
+              key="IC"
               icon={<CreditCardOutlined />}
               disabled
             >
-              Store
-            </Menu.Item>
-            <Menu.Item
-              key="settings-options"
-              icon={<CreditCardOutlined />}
-              disabled
-            >
-              Options
-            </Menu.Item>
-            <Menu.Item
-              key="settings-hours"
-              icon={<CreditCardOutlined />}
-              disabled
-            >
-              Hours
+              Info Cards
             </Menu.Item>
           </Menu.ItemGroup>
         </Menu>
-        <div className="border-0 p-4 bg-gray-200">
+        <div className="border-0 p-4 bg-gray-200 flex justify-center">
           <Button
-            shape="circle"
-            type="primary"
+            type="danger"
             onClick={() => setCollapsed(true)}
           >
-            Primary Button
+            Close
           </Button>
         </div>
       </div>
